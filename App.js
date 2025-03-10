e070c4dc4c83fe785b1eee9e701a2e33f4e5ec6f
franson-dev <welintonffranson@icloud.com>
2025-03-10 20:46:36 -0300

    Atualização do codigo para o TextInput e adicionando para os outros locais sem ser o CEP

--- App.js
+++ App.js
@@ -1,18 +1,15 @@
-import { Text, SafeAreaView, StyleSheet, TextInput, Button } from 'react-native';
-import { useState } from 'react';
-
-
-
+import * as React from 'react';
+import { View, StyleSheet } from 'react-native';
+import { PaperProvider, TextInput, Text, Button } from 'react-native-paper';
 
 export default function App() {
-  // cep = ;
-  let [cep, setCep] = useState(null);
-  let [render, setRender] = useState([]);
+  let [cep, setCep] = React.useState("");
+  let [render, setRender] = React.useState([]);
 
-
-  let BuscaCep = (xcep) => {
+  const BuscaCep = (xcep) => {
     let url = `https://viacep.com.br/ws/${xcep}/json/`;
     console.log(url);
+
     fetch(url)
       .then(
         (resp) => { return resp.json() }
@@ -21,7 +18,7 @@ export default function App() {
           console.log(dados);
 
           console.log(dados.logradouro);
-          console.log(dados["logradouro"]);
+          console.log(dados['logradouro']);
           setRender(dados);
 
         }
@@ -31,39 +28,23 @@ export default function App() {
   }
 
   return (
-    <SafeAreaView style={styles.container}>
-      <Text>Cep</Text>
-      <TextInput
-        onChangeText={(value) => { setCep(value) }}
-        placeholder="Digite Cep"
-      />
-      <Button
-        title="Buscar"
-        onPress={() => { BuscaCep(cep) }}
-      />
+    <PaperProvider>
+      <View style={{ marginTop: 50, marginHorizontal: 20 }}>
+        <Text style={{ color: '#A7C' }}>ViaCEP</Text>
+        <TextInput style={{ backgroundColor: '#FFF' }} label={'CEP:'} mode='outlined' onChangeText={(value) => { setCep(value) }} />
+        <Button icon={"tab-search"} onPress={() => { BuscaCep(cep) }}
+          mode='contained' style={{ marginTop: 20 }}>Busca</Button>
 
-      <Text> Endereço : {render["logradouro"]}</Text>
-      <Text> Bairro : {render["bairro"]}</Text>
-      <Text> Cidade : {render.localidade}</Text>
-      <Text> Estado : {render.estado}</Text>
+        <TextInput label={'Endereço: '} value={render['logradouro']} mode='outlined' onChangeText={(value) => { setCep(value) }} />
+        <TextInput label={'Bairro: '} value={render['bairro']} mode='outlined' onChangeText={(value) => { setCep(value) }} />
+        <TextInput label={'Cidade: '} value={render['localidade']} mode='outlined' onChangeText={(value) => { setCep(value) }} />
+        <TextInput label={'Estado: '} value={render['estado']} mode='outlined' onChangeText={(value) => { setCep(value) }} />
 
+        <Button icon={"tab-"} onPress={() => { BuscaCep(cep) }}
+          mode='contained' style={{ marginTop: 20 }}>Busca</Button>
 
-    </SafeAreaView>
+      </View>
+    </PaperProvider>
   );
-}
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-
-    padding: 8,
-    margin: 20
-  },
-  paragraph: {
-    margin: 24,
-    fontSize: 18,
-    fontWeight: 'bold',
-    textAlign: 'center',
-  },
-});
+};  
